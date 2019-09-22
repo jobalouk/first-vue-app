d4b07e8560bae7945a1413dfeded7ef7ad500711
Joackim Balouka <jobalouka@gmail.com>
2019-09-22 20:11:17 +0200

    first vue app

---
+++ src/components/TodoList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <BaseInputText
+      v-model="newTodoText"
+      placeholder="New todo"
+      @keydown.enter="addTodo"
+    />
+    <ul v-if="todos.length">
+      <TodoListItem
+        v-for="todo in todos"
+        :key="todo.id"
+        :todo="todo"
+        @remove="removeTodo"
+      />
+    </ul>
+    <p v-else>
+      Nothing left in the list. Add a new todo in the input
+    </p>
+  </div>
+</template>
+
+<script>
+  import BaseInputText from './BaseInputText.vue'
+  import TodoListItem from './TodoListItem.vue'
+
+  let nextTodoId = 1
+
+  export default {
+    components: {
+      BaseInputText, TodoListItem
+    },
+    data () {
+      return {
+        newTodoText: '',
+        todos: [
+          {
+              id: nextTodoId++,
+              text: 'Learn Vue',
+          },
+          {
+              id: nextTodoId++,
+              text: 'Learn about single-file components',
+          },
+        ]
+      }
+    },
+    methods: {
+      addTodo () {
+        const trimmedText = this.newTodoText.trim()
+        if (trimmedText) {
+          this.todos.push({
+            id: nextTodoId++,
+            text: trimmedText
+          })
+          this.newTodoText = ''
+        }
+      },
+      removeTodo (idToRemove) {
+        this.todos = this.todos.filter(todo => {
+          return todo.id !== idToRemove
+        })
+      }
+    }
+  }
+</script>
